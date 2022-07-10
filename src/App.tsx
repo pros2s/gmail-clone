6ba928fd0520715d4407f8cd3a759ef2697d0433
Alexandr <caiiia_podoplelov@mail.ru>
2022-07-10 18:52:54 +0300

    added some routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,26 @@
 import React from 'react';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 
-import MessageList from './components/messageList';
+import Inbox from './pages/Inbox';
+import MessageContent from './pages/MessageContent';
 
 
 function App() {
   return (
     <div>
-      <MessageList />
+      <BrowserRouter>
+        <Routes>
+          <Route path='/Inbox' element={ <Inbox /> } />
+          <Route path='/Sent' element={ <Inbox /> } />
+          <Route path='/Draft' element={ <Inbox /> } />
+          <Route path='/Deleted' element={ <Inbox /> } />
+          <Route path='/Spam' element={ <Inbox /> } />
+
+          <Route path='/:folder/:id' element={ <MessageContent /> } />
+          <Route path='/' element={ <Navigate replace to='/Inbox'/> } />
+        </Routes>
+
+      </BrowserRouter>
     </div>
   );
 };
