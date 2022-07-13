886cf824d36c1443f29314f876fdbce524d0609b
Alexandr <caiiia_podoplelov@mail.ru>
2022-07-13 03:06:22 +0300

    right side settings

--- src/components/message/Message.tsx
+++ src/components/message/Message.tsx
@@ -1,11 +1,16 @@
 import React, { FC, MouseEvent, useEffect, useState } from 'react';
 import { useNavigate, useParams } from 'react-router-dom';
+import classNames from 'classnames';
 import {
   RiBookmark3Fill,
   RiDeleteBinFill,
   RiSpamFill,
   RiCheckboxCircleFill,
-  RiArrowGoBackFill
+  RiArrowGoBackFill,
+  RiMore2Fill,
+  RiMailCheckLine,
+  RiFolderSharedLine,
+  RiFolder2Fill
 } from 'react-icons/ri';
 
 import { randomDate } from '../../redux/slices/ActionCreators';
@@ -13,6 +18,7 @@ import { IMessage } from '../../types/message';
 import './message.scss';
 import { addChoosed, removeById } from '../../redux/slices/chosenMessages';
 import { useAppDispatch } from '../../hooks/useAppDispatch';
+import { useAppSelector } from '../../hooks/useTypedSelector';
 
 
 interface IMessageProps {
@@ -21,25 +27,27 @@ interface IMessageProps {
 
 const Message: FC<IMessageProps> = ({ message }) => {
   const { name, company, id } = message;
-
-  const dispatch = useAppDispatch();
+  
   const { folder } = useParams();
   const route = useNavigate();
 
+  const dispatch = useAppDispatch();
+  const { customFolders } = useAppSelector((state) => state.customFoldersReducer);
+
   const [ folderNames, setFolderNames ] = useState([ 'Inbox' ]);
   const [ randDate, setRandDate ] = useState('');
+
   const [ isMarked, setIsMarked ] = useState(false);
   const [ isChecked, setIsChecked ] = useState(false);
+  const [ isMore, setIsMore ] = useState(false);
+  const [ isRead, setIsRead ] = useState(false);
+  const [ isShowModal, setIsShowModal ] = useState(false);
 
   useEffect(() => {
-    setDate();
-  }, []);
-
-
-  const setDate = () => {
     setRandDate(randomDate('02/13/2022', '01/01/2000'));
     // setRandDate(randomDate());
-  };
+  }, []);
+
 
   const onClickCheck = (e: MouseEvent<SVGElement>) => {
     e.stopPropagation();
@@ -61,25 +69,59 @@ const Message: FC<IMessageProps> = ({ message }) => {
     setIsMarked((state) => !state);
   };
 
-  const onClickRightBtns = (e: MouseEvent<SVGElement>, folderName: string) => {
+  const onClickRightBtns = (e: MouseEvent<SVGElement | HTMLDivElement>, folderName: string) => {
     e.stopPropagation();
 
-    if (folderName === 'Inbox') {
-      setFolderNames((state) => [ ...state, 'Inbox'].filter((folder) => folder !== 'Deleted' && folder !== 'Spam'));
+    if (folderName !== 'Deleted' && folderName !== 'Spam') {
+      setFolderNames((state) => [ ...state, folderName ].filter((folder) => folder !== 'Deleted' && folder !== 'Spam'));
+      setIsShowModal(false);
     }
     else {
       folder !== folderName && setFolderNames((state) => [ ...state, folderName ].filter((folder) => folderName === folder));
     }
   };
 
+  const onClickMore = (e: MouseEvent<SVGElement>) => {
+    e.stopPropagation();
+    setIsMore((state) => !state);
+  };
+
+  const markAsRead = (e: MouseEvent<HTMLDivElement>) => {
+    e.stopPropagation();
+    setIsRead(true);
+    setIsMore(false);
+  };
+
+  const showFoldersMenu = (e: MouseEvent<HTMLDivElement>) => {
+    e.stopPropagation();
+    customFolders.length !== 0 && setIsShowModal((state) => !state);
+  };
+
+  const onLeaveRightTools = () => {
+    setIsMore(false);
+    setIsShowModal(false);
+  };
+
+  const onClickMessage = () => {
+    setIsRead(true);
+    route(`/mailclone/${ folder }/${ id }`);
+  };
+
+  const messageClassName = classNames({
+    'message': true,
+    'active': isMore,
+    'notRead': !isRead
+  });
+
 
   return (
     <>
       {
         folder && folderNames.includes(folder) &&
+
         <div
-          className='message'
-          onClick={ () => route(`/mailclone/${ folder }/${ id }`)}>
+          className={ messageClassName }
+          onClick={ () => onClickMessage() }>
             <div className="message__tools-left d-flex">
               <RiCheckboxCircleFill
                 title='choose'
@@ -94,14 +136,51 @@ const Message: FC<IMessageProps> = ({ message }) => {
             <p className='message__name'>{ name }</p>
             <p className='message__preview'>{ company.name }</p>
             <p className='message__date'>{ randDate }</p>
+
             <div className="message__tools-right">
               {
-                folder === 'Deleted' || folder === 'Spam' ?
-                <RiArrowGoBackFill onClick={ (e) => onClickRightBtns(e, 'Inbox') } /> :
-                <>
-                  <RiDeleteBinFill title='delete' onClick={ (e) => onClickRightBtns(e, 'Deleted') } />
-                  <RiSpamFill title='spam' onClick={ (e) => onClickRightBtns(e, 'Spam') } />
-                </>
+                folder === 'Deleted' || folder === 'Spam'
+                  ? <RiArrowGoBackFill onClick={ (e) => onClickRightBtns(e, 'Inbox') } />
+                  :
+                  <>
+                    <RiDeleteBinFill style={{ marginRight: 3 }} title='delete' onClick={ (e) => onClickRightBtns(e, 'Deleted') } />
+                    <RiSpamFill title='spam' onClick={ (e) => onClickRightBtns(e, 'Spam') } />
+
+                    <div className="message__tools-right-more d-flex">
+                      <RiMore2Fill
+                        title='more'
+                        onClick={ (e) => onClickMore(e) }/>
+                      <div
+                        className={ isMore ? 'message__tools-right-more-menu active' : 'message__tools-right-more-menu' }
+                        onMouseLeave={ () => onLeaveRightTools() }>
+                          <div className='message__tools-right-more-menu-choose'>
+                            <div className='d-flex ai-center' onClick={ (e) => showFoldersMenu(e) }>
+                              <p>add to</p>
+                              <RiFolderSharedLine />
+                            </div>
+
+                            <div className='d-flex ai-center' onClick={ (e) => markAsRead(e) }>
+                              <p>mark as read</p>
+                              <RiMailCheckLine />
+                            </div>
+                          </div>
+
+                          <div className={
+                            isShowModal
+                              ? 'message__tools-right-more-menu-folders active'
+                              : 'message__tools-right-more-menu-folders' }>
+                                {
+                                  customFolders.map((folder) => (
+                                    <div key={ folder } className='d-flex ai-center' onClick={ (e) => onClickRightBtns(e, folder) }>
+                                      <RiFolder2Fill />
+                                      <p>{ folder }</p>
+                                    </div>
+                                  ))
+                                }
+                          </div>
+                      </div>
+                    </div>
+                  </>
               }
             </div>
         </div>
