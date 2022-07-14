ac10f939a3d332c6460808ba5539934c6917fea5
Alexandr <caiiia_podoplelov@mail.ru>
2022-07-14 20:26:08 +0300

    finish

---
+++ src/components/footer/Footer.tsx
@@ -0,0 +1,30 @@
+import React, { FC } from 'react';
+import { RiGithubFill } from 'react-icons/ri';
+
+import './footer.scss';
+
+
+const Footer: FC = () => {
+  return (
+    <div className='footer'>
+      <a
+        href='https://github.com/pros2s'
+        className="footer__github"
+        target='_blank'
+        rel="noreferrer" >
+          <RiGithubFill />
+      </a>
+
+      <a
+        href='https://alexandr-portfolio.netlify.app/'
+        className="footer__sign"
+        target='_blank'
+        rel="noreferrer" >
+          <p>@2022 ALEXANDR</p>
+      </a>
+    </div>
+  );
+};
+
+
+export default Footer;
